321f187254b85389c2d616a489748dcc8671bda8
Jack Dunn <jack.dunn.nz@gmail.com>
2015-01-17 20:37:18 -0500

    Add basic categorical implementation that works for binary values

--- convert.py
+++ convert.py
@@ -25,6 +25,7 @@ def process_dir(data_path, processed_path, normalise=True):
   dataset_categoric_indices = parser.get('info', 'categoric_indices')
   dataset_separator = parser.get('info', 'separator')
   dataset_header_lines = parser.getint('info', 'header_lines')
+  dataset_encoding = parser.get('info', 'encoding')
 
   dataset_id_indices = convert_list(dataset_id_indices)
   dataset_value_indices = convert_list(dataset_value_indices)
@@ -38,6 +39,11 @@ def process_dir(data_path, processed_path, normalise=True):
     urllib.urlretrieve(dataset_url, dataset_path)
 
   f_in = open(dataset_path, 'rU')
+  lines = f_in.read().decode(dataset_encoding)
+  lines = [line for line in lines.splitlines() if line]
+  lines = lines[dataset_header_lines:]
+  f_in.close()
+
 
   classes = {}
   class_count = 0
@@ -53,74 +59,78 @@ def process_dir(data_path, processed_path, normalise=True):
   row_classes = []
   row_ids = []
 
-  lines = f_in.readlines()
-  lines = lines[dataset_header_lines:]
 
   for i, line in enumerate(lines):
-      if dataset_separator:
-          lines[i] = line.strip('\n').split(dataset_separator)
+    if dataset_separator:
+      lines[i] = line.strip('\n').split(dataset_separator)
+    else:
+      lines[i] = line.strip('\n').split()
+
+    row_class = lines[i][dataset_class_index]
+    if row_class not in classes:
+      classes[row_class] = class_count
+      class_count += 1
+    row_classes.append(classes[row_class])
+
+    if dataset_id_indices:
+      row_ids.append('_'.join([lines[i][j] for j in dataset_id_indices]))
+    else:
+      row_ids.append('id_%i' % i)
+    v = [lines[i][j] for j in dataset_value_indices]
+
+    for j, val in enumerate(v):
+      if dataset_value_indices[j] in categoric_set:
+        if val not in categoric_classes[j]:
+          categoric_classes[j][val] = categoric_counts[j]
+          categoric_counts[j] += 1
       else:
-          lines[i] = line.strip('\n').split()
+        try:
+          val = float(val)
+        except:
+          val = float(val.replace(',', '.'))
+          lines[i][dataset_value_indices[j]] = val
+        if max_val[j] < val:
+          max_val[j] = val
+        if min_val[j] > val:
+          min_val[j] = val
 
-      row_class = lines[i][dataset_class_index]
-      if row_class not in classes:
-        classes[row_class] = class_count
-        class_count += 1
-      row_classes.append(classes[row_class])
+  for line in lines:
+    new_values = []
+    for j in dataset_value_indices:
+      if j in dataset_categoric_indices:
+        # convert n categorical to n-1 dummy vars
+        categoric = [0] * (categoric_counts[j] - 1)
+        cur_index = categoric_classes[j][line[j]] - 1
+        if cur_index >= 0:
+          categoric[cur_index] = 1
+        new_values += categoric
 
-      if dataset_id_indices:
-          row_ids.append('_'.join([lines[i][j] for j in dataset_id_indices]))
       else:
-          row_ids.append('id_%i' % i)
-
-      v = [lines[i][j] for j in dataset_value_indices]
-
-      for j, val in enumerate(v):
-          if dataset_value_indices[j] in categoric_set:
-              if val not in categoric_classes[j]:
-                  categoric_classes[j][val] = categoric_counts[j]
-                  categoric_counts[j] += 1
-              lines[i][dataset_value_indices[j]] = categoric_classes[j][val]
-          else:
-              val = float(val)
-              if max_val[j] < val:
-                  max_val[j] = val
-              if min_val[j] > val:
-                  min_val[j] = val
-
-  for i, j in enumerate(dataset_value_indices):
-      if j in categoric_set:
-          max_val[i] = categoric_counts[j] - 1
-          min_val[i] = 0
-
-  # Normalise
-  if normalise:
-      for line in lines:
-          values = [line[j] for j in dataset_value_indices]
-          row_values.append([str((float(x) - z) / float(y - z))
-                             for x, y, z in zip(values, max_val, min_val)])
-  else:
-      for line in lines:
-          row_values.append([line[j] for j in dataset_value_indices])
+        if normalise:
+          new_values.append(str((float(line[j]) - min_val[j]) /
+                                float(max_val[j]  - min_val[j])))
+        else:
+          new_values.append(line[j])
+    row_values.append(new_values)
 
   f = []
   for i, j in combinations(classes.itervalues(), 2):
-      outname = '%s.%s' % (dataset_name, str(i) + '.' + str(j))
-      f.append(open(os.path.join(processed_path, outname), 'w'))
+    outname = '%s.%s' % (dataset_name, str(i) + '.' + str(j))
+    f.append(open(os.path.join(processed_path, outname), 'w'))
 
   for i, line in enumerate(lines):
-      row_class = row_classes[i]
-      row_id = row_ids[i]
-      out_string = ','.join([row_id] + row_values[i]) + ','
+    row_class = row_classes[i]
+    row_id = row_ids[i]
+    out_string = ','.join([row_id] + map(str, row_values[i])) + ','
 
-      for i, comb in enumerate(combinations(classes.itervalues(), 2)):
-          if row_class in comb:
-              row_class_new = 0 if row_class == comb[0] else 1
-              f[i].write(out_string + str(row_class_new) + '\n')
+    for i, comb in enumerate(combinations(classes.itervalues(), 2)):
+      if row_class in comb:
+        row_class_new = 0 if row_class == comb[0] else 1
+        f[i].write(out_string + str(row_class_new) + '\n')
 
-  f_in.close()
+
   for f_out in f:
-      f_out.close()
+    f_out.close()
 
 def main(normalise=False):
 
