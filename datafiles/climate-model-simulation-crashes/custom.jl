9fd308704fb6c2971b7141977b782d2c23ec57f1
Jack Dunn <jack.dunn.nz@gmail.com>
2015-01-21 20:31:59 -0500

    Tidy up comments

--- datafiles/climate-model-simulation-crashes/custom.jl
+++ datafiles/climate-model-simulation-crashes/custom.jl
@@ -1,16 +1,7 @@
-# The , as a decimal separator isn't yet supported by DataFrames
-# We replace all , with . as a workaround
+# The whitespace in this file is weird. Pass through tr first, then remove
+# extra commas
 
 dataset_path = "datafiles/climate-model-simulation-crashes/climate-model-simulation-crashes.data.orig"
-
-# f = open(dataset_path)
-# data = readbytes(f)
-# close(f)
-
-# println(data)
-
-# data = replace(data, ",", ".")
-
 
 data = readlines(`cat $dataset_path` |> `tr -s '[:blank:]' ','`)
 
@@ -21,6 +12,7 @@ for line in data
   if line[1] == ','
     line = line[2:end]
   end
+  # Trim trailing comma
   if line[end] == ','
     line = line[1:end - 1]
   end
