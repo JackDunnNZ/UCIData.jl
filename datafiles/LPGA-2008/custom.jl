773904e81898c8e6b48b380642e8794643db1024
Aris Paschalidis <aris.paschalidis@gmail.com>
2016-06-13 13:06:26 -0400

    Added LPGA 2008 data

---
+++ datafiles/LPGA-2008/custom.jl
@@ -0,0 +1,10 @@
+#data set has names at the beginning of it. Since the parts of the names have spaces in between them and a space is the seperator for the
+#rest of the file, it gets confused how mnany colums exist
+
+dataset_path = "datafiles/LPGA-2008/LPGA-2008.data.orig"
+
+f = open("$dataset_path.custom", "w")
+for line in readlines(dataset_path)
+  write(f, line[31:end])
+end
+close(f)
