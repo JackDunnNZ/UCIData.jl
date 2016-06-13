958f0e828db87259b0de3de7bc4619f497187506
Aris Paschalidis <aris.paschalidis@gmail.com>
2016-06-13 14:19:25 -0400

    Fixed a spelling error on the custom.jl file of the 2008 LPGA data

--- datafiles/LPGA-2008/custom.jl
+++ datafiles/LPGA-2008/custom.jl
@@ -1,5 +1,5 @@
 #data set has names at the beginning of it. Since the parts of the names have spaces in between them and a space is the seperator for the
-#rest of the file, it gets confused how mnany colums exist
+#rest of the file, it gets confused how many colums exist
 
 dataset_path = "datafiles/LPGA-2008/LPGA-2008.data.orig"
 
