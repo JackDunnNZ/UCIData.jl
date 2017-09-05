ad246ea0ea965b14299526afd0c18e640443543e
Ying Zhuo <zhuo@dhcp-18-189-82-223.dyn.MIT.EDU>
2017-09-05 13:31:32 -0400

    add function to keep categorical

--- runconvert.jl
+++ runconvert.jl
@@ -8,6 +8,9 @@ function main()
     "--normalize", "-n"
       help = "set if the data is to be normalized"
       action = :store_true
+    "--keepcat", "-k"
+      help = "optional: categorical coding C1, C2, etc.. Do not dummify. "
+      action = :store_true
     "--class_size", "-c"
       help = "optional: the number of classes to use in output datasets. " *
              "Leave as zero to include all classes. Applies to " *
@@ -20,14 +23,16 @@ function main()
              "Applies to classification only."
       arg_type = Int
       default = 0
+
   end
   parsed_args = parse_args(s)
   normalize = parsed_args["normalize"]
+  keepcat = parsed_args["keepcat"]
   class_size = parsed_args["class_size"]
   min_size = parsed_args["min_size"]
 
-  processalldirs("classification", normalize, class_size, min_size)
-  processalldirs("regression", normalize)
+  processalldirs("classification", normalize, keepcat, class_size, min_size)
+  processalldirs("regression", normalize, keepcat)
 end
 
 main()
