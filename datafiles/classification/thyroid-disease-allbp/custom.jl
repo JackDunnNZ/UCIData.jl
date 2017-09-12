319b4218f6ace63cbb94649ca043a281212df78c
daisy12321 <candydaisy12321@gmail.com>
2017-09-12 16:21:25 -0400

    Fix all readlines problems

--- datafiles/classification/thyroid-disease-allbp/custom.jl
+++ datafiles/classification/thyroid-disease-allbp/custom.jl
@@ -3,13 +3,5 @@
 
 dataset_path = joinpath(dirname(@__FILE__), "thyroid-disease-allbp.data.orig")
 
-f = open(dataset_path)
-data = readlines(f)
-close(f)
-
-f = open("$dataset_path.custom", "w")
-for line in data
-  line = replace(line, ".|", ",")
-  write(f, line)
-end
-close(f)
+data = readstring(dataset_path)
+write("$dataset_path.custom", replace(data, ".|", ","))
