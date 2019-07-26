69e9f79dad737ee4147cf7c9f0093171554e9256
daisy12321 <candydaisy12321@gmail.com>
2019-07-26 13:39:06 -0400

    Upgrade to DataFrames v0.19.0

--- src/convert.jl
+++ src/convert.jl
@@ -27,7 +27,7 @@ function process_dataset(
   output_names = Symbol[]
   output_df = DataFrame()
 
-  output_df[:id] = [makeid(df, i, id_indices) for i in 1:size(df, 1)]
+  output_df[!, :id] = [makeid(df, i, id_indices) for i in 1:size(df, 1)]
   push!(output_names, :id)
 
   # Construct output values
@@ -38,11 +38,11 @@ function process_dataset(
     else
       push!(output_names, :N)
     end
-    output_df[size(output_df, 2) + 1] = df[i]
+    output_df = hcat(output_df, df[!, i], makeunique=true)
   end
   names!(output_df, output_names, makeunique=true)
 
-  output_df[:target] = df[target_index]
+  output_df[!, :target] = df[!, target_index]
 
   output_path = joinpath(dirname(path), "data.csv")
 
