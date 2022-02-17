9b14f8db85a7e5e61d0312c4f717f1ebae82204b
Jack Dunn <JackDunnNZ@users.noreply.github.com>
2022-02-17 09:24:49 -0500

    Fix arrhythmia target

--- src/data/classification/arrhythmia.jl
+++ src/data/classification/arrhythmia.jl
@@ -7,7 +7,7 @@ register(DataDep(
   "a7f0f4ca289a4c58b5ed85a9adb793189acd38425828ce3dfbb70adb45f30169",
   post_fetch_method=(path -> begin
     UCIData.process_dataset(path,
-        target_index=200,
+        target_index=280,
         feature_indices=1:279,
     )
   end),
