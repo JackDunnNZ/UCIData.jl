24e8bcfcbda6ec06c5f63507641762b32f66a7c5
Jack Dunn <jack.dunn.nz@gmail.com>
2023-02-13 13:14:14 -0500

    Fix UTF-16 reading

--- src/data/classification/acute-inflammations-1.jl
+++ src/data/classification/acute-inflammations-1.jl
@@ -6,8 +6,12 @@ register(DataDep(
   "http://archive.ics.uci.edu/ml/machine-learning-databases/acute/diagnosis.data",
   "5d0b61049992b8327aa847f916f3389ec08f74b43dbb3fc5222ac07185d7c88f",
   post_fetch_method=(path -> begin
-    # File is UTF-16 and first 2 bytes are garbage
-    data = transcode(String, reinterpret(UInt16, read(path)[3:end]))
+    # File is UTF-16 and first 2 bytes are BOM
+    utf16 = reinterpret(UInt16, read(path))
+    if utf16[1] == 0xfffe # BOM indicates non-native endianness
+      utf16 .= bswap.(utf16)
+    end
+    @views data = transcode(String, utf16[2:end])
     data = replace(data, "," => ".")
     write(path, data)
 
