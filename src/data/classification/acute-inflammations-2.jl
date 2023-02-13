130ce2f73a44b8e1fb5e08cc9b133b266a491896
Jack Dunn <JackDunnNZ@users.noreply.github.com>
2023-02-13 10:59:02 -0500

    Remove LegacyStrings (#35)
    Fix #34

--- src/data/classification/acute-inflammations-2.jl
+++ src/data/classification/acute-inflammations-2.jl
@@ -6,8 +6,8 @@ register(DataDep(
   "http://archive.ics.uci.edu/ml/machine-learning-databases/acute/diagnosis.data",
   "5d0b61049992b8327aa847f916f3389ec08f74b43dbb3fc5222ac07185d7c88f",
   post_fetch_method=(path -> begin
-    # First 2 bytes are garbage
-    data = LegacyStrings.utf16(read(path)[3:end])
+    # File is UTF-16 and first 2 bytes are garbage
+    data = transcode(String, reinterpret(UInt16, read(path)[3:end]))
     data = replace(data, "," => ".")
     write(path, data)
 
