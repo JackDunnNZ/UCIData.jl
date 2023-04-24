aba8f844475f3f4e4428481a9f2482c3c5736420
Jack Dunn <jack.dunn.nz@gmail.com>
2023-04-24 09:27:40 -0400

    Fix breast-cancer target column

--- src/data/classification/breast-cancer.jl
+++ src/data/classification/breast-cancer.jl
@@ -7,9 +7,9 @@ register(DataDep(
   "ca7d3fa97b62ff967b6894ffbb3acaaf4a51062e0149e5950b3ad6f685970b65",
   post_fetch_method=(path -> begin
     UCIData.process_dataset(path,
-        target_index=10,
-        feature_indices=1:9,
-        categoric_indices=[1:6; 8:9],
+        target_index=1,
+        feature_indices=2:10,
+        categoric_indices=[2:6; 8:10],
     )
   end),
 ))
